c8c4de2ad7fece075b38ebfa26ccc39eef15e450
JoacoEducabot <joaquin.baldo@gmail.com>
2026-04-17 14:39:17 -0300

    Refactor for User creation: Now the entity contains name,  and errors are shown in status instead of response.

--- Prode/src/contracts/request/users_request.py
+++ Prode/src/contracts/request/users_request.py
@@ -2,7 +2,7 @@ from entities.users import User
 
 def create_user_request(user: User) -> dict: 
     return {
-        "email": user["email"],
-        "name": user["name"],
-        "password": user["password"]
+        "email": user.get("email"),
+        "name": user.get("name"),
+        "password": user.get("password")
     }
