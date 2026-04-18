5a75d4ad335a94c0e5f922b25c76ede34c539d02
Hilario <hilariorosi54@gmail.com>
2026-04-18 16:06:30 -0300

    Limpieza final de comentarios

--- Prode/src/infrastructure/entrypoints/users/delete_user.py
+++ Prode/src/infrastructure/entrypoints/users/delete_user.py
@@ -1,7 +1,6 @@
 from src.repository.users.delete_user_core import delete_user_core
 
 def execute(user_id: int) -> dict:
-    # Esta es la función que limpia todas las tablas de la DB
     success, err = delete_user_core(user_id)
     if err is not None:
         return err
