726f3f0f92621c11c93fff89ad04d369f3e30e50
Ezequiel Di Sabatino <jdisabatino@fi.uba.ar>
2026-04-19 12:52:51 -0300

    crea una carpeta /ranking en usecases. mueve la funcion get_users_ranking a entrypints.ranking . Y modifica la app.py para que pueda llamarlas correctamente

--- Prode/src/app.py
+++ Prode/src/app.py
@@ -3,14 +3,16 @@ import sys
 import os
 from infrastructure.entrypoints.users import users
 from infrastructure.entrypoints.partidos import partidos
-from flask import Flask, Blueprint
+from infrastructure.entrypoints.ranking.ranking import get_users_ranking
+from flask import Flask, Blueprint, jsonify
 
 app = Flask(__name__)
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 users_bp = Blueprint('users', __name__, url_prefix='/users')
 partidos_bp = Blueprint('partidos', __name__, url_prefix='/partidos')
+ranking_bp = Blueprint('ranking', __name__, url_prefix='/ranking')
 
 @users_bp.route('/register', methods=['POST'])
 def create_user_endpoint():
@@ -27,8 +29,15 @@ def put_replace_partido_endpoint(partido_id: int):
     return partidos.put_replace_partido(partido_id)
 
 
+@ranking_bp.route('/', methods=['GET'])
+def get_ranking_endpoint():
+    ranking_data = get_users_ranking()
+    return jsonify(ranking_data)
+
+
 app.register_blueprint(users_bp)
 app.register_blueprint(partidos_bp)
+app.register_blueprint(ranking_bp)
 
 
 
