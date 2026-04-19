cc829083826639a357fecb735406cf56fd6cfbf8
Ezequiel Di Sabatino <jdisabatino@fi.uba.ar>
2026-04-19 12:19:23 -0300

    añade una constante para el limite de la paginacion

--- Prode/src/usecases/users/get_users_list.py
+++ Prode/src/usecases/users/get_users_list.py
@@ -1,7 +1,8 @@
 from repository.users.get_users import get_users_list
 from contracts.response.users_response import get_users_list_response
 
+LIMIT = 10
 
-def execute(page: int = 1, limit: int = 10):
+def execute(page: int = 1, limit: int = LIMIT):
     users, total = get_users_list(page, limit)
     return get_users_list_response(users, total, page, limit)
